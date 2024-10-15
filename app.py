d10f7f41b2e7e64d7c7d57335cefdf2a27a84c0c
Ghulam Haider <155440080+ghulamhaider11@users.noreply.github.com>
2024-10-16 00:24:25 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,56 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
-import ydata_profiling
-from streamlit_pandas_profiling import st_profile_report
+import pandas as pd
+import streamlit as st
+#from pandas_profiling import ProfileReport
 from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
+# Web App Title
+st.markdown('''
+# **The EDA App**
 
-st.title("Data Profiling App")
-st.subheader("This app will help you to do Data Exploration")
+This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
 
-st.sidebar.header('User Input Features')
+**Credit:** App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
 
+---
+''')
 
-# Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input Excel or CSV file", type=["xlsx", "csv"])
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
 
+# Pandas Profiling Report
 if uploaded_file is not None:
-    st.markdown('---')
-    
-    # Check file extension and load data accordingly
-    if uploaded_file.name.endswith('.xlsx'):
-        input_df = pd.read_excel(uploaded_file, engine="openpyxl")
-    elif uploaded_file.name.endswith('.csv'):
-        input_df = pd.read_csv(uploaded_file)
-    
-    profile = ProfileReport(input_df, title="New Data for profiling")
-
-    st.subheader("Detailed Report of the Data Used")
-
-    st.write(input_df)
-
-    st_profile_report(profile)    
-    
+    @st.cache_data
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
 else:
-    st.write("You did not upload a file.")
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        # Example data
+        @st.cache_data
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['a', 'b', 'c', 'd', 'e']
+            )
+            return a
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
